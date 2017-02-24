495e146ea6aacc669fbad5a7cd6e0497a5d8476b
sheyshey22 <s.vilca.neira@gmail.com>
2017-02-24 06:24:25 -0500

    portfolio init

--- js/creative.js
+++ js/creative.js
@@ -1,53 +1,3 @@
-/*!
- * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Initialize WOW.js Scrolling Animations
-    new WOW().init();
-
-})(jQuery); // End of use strict
-
-
 var paperMenu = {
     $window: $('#paper-window'),
     $paperFront: $('#paper-front'),
@@ -99,23 +49,9 @@ var paperMenu = {
         this.bindEvents();
         this.updateTransformOrigin();
     },
+
+
+
 };
-
-$(function () { 
-  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
-});  
-
-// $( window ).scroll(function() {   
- // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
-  $(".progress-bar").each(function(){
-    each_bar_width = $(this).attr('aria-valuenow');
-    $(this).width(each_bar_width + '%');
-  });
-       
- //  }  
-// });
-
-
-
-
 paperMenu.init();
+
